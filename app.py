939ccb64fa9ff462ed62230d4ce499e135f7a696
Purva Waykole <113341495+purvacode-80@users.noreply.github.com>
2024-08-25 00:24:01 +0530

    Update app.py
    host

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def translate():
     return render_template('index.html', translations=formatted_translations)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host="0:0:0:0)
